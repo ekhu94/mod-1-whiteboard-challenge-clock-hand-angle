16c50d01767cc44ac3154077fee11c4440dd347b
Erik Huang <erikhuangdrums@gmail.com>
2021-01-26 09:58:41 -0800

    commit tree 627a90bd479d851413490032d9b821f99aa37cfe

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,18 @@
 def clock_angle(time)
-    # code your solution here
+    hour = time.split(":")[0].to_i
+    min = time.split(":")[1].to_i
+    hour = 0 if hour == 12
+    min_deg = min * 6
+    hour_deg = hour * 30
+    degree = (hour_deg - min_deg) + min / 2.0
+    degree < 0 ? degree + 360 : degree
 end
+
+#minute degree: 6 * mins
+#hour degree: 30 * hour
+# 1:15
+# min_deg - hour_deg
+# 0 - 0
+# 3:15
+
+# 16
